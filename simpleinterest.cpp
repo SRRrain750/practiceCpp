6b566ad6572f30ff02a22bfdde1eced131dc78e8
SRRrain750 <160513264+SRRrain750@users.noreply.github.com>
2024-05-03 22:56:34 +0530

    Add files via upload

---
+++ simpleinterest.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main(){
+
+double principal=1200,time=2,rate=3;
+double SI=(principal*time*rate/100);
+cout<<"simple interest is:"<<SI<<endl;
+return 0;
+}
